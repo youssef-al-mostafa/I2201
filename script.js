3e01d2bd6e55b47b6f2e2eca656446df9a5bd4f5
youssef-al-mostafa <youssefalmostafa2@gmail.com>
2023-05-04 19:33:04 +0300

    Commit message

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var textarea = document.getElementById("textarea");
+    var submit = document.getElementById("submittextarea");
+    
+    submit.addEventListener("click", function() {
+      if (textarea.value != "") {
+        alert("Thank you for taking the time to share your feedback with us. We value your input and appreciate the opportunity to improve our service.");
+      }
+    });
+  });
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+
+  
